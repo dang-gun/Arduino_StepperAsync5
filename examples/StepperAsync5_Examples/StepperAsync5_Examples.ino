916fdfc267fe02b53259ca61400409b4b1d912f6
dang-gun <c1dong8@gmail.com>
2023-06-08 10:11:32 +0900

    예제 수정

--- examples/StepperAsync5_Examples/StepperAsync5_Examples.ino
+++ examples/StepperAsync5_Examples/StepperAsync5_Examples.ino
@@ -1,48 +1,35 @@
 #include <StepperAsync5.h>
- 
-int LED1 = A0;
- 
+
 StepperAsync5 stepper(200, 12, 11, 10, 9);
 StepperAsync5 stepper2(200, 7, 6, 5, 4);
- 
+
 void setup() 
 {
   Serial.begin(9600);
- 
-  pinMode(LED1, OUTPUT);
-  
   stepper.setSpeed(30);
   stepper2.setSpeed(60);
 }
- 
+
 void loop() 
 {
   if (Serial.available())
   {
     int steps = Serial.parseInt();
- 
+
     if(steps != 0)
     {
-      digitalWrite(LED1, HIGH);
       stepper.setStep(steps);
-      digitalWrite(LED1, LOW);
       stepper2.setStep(steps);
-      digitalWrite(LED1, HIGH);
       //stepper.setStep(200);
       //stepper2.setStep(200);
- 
+  
       Serial.print("steps : ");
       Serial.println(steps); 
-      delay(100);
-      digitalWrite(LED1, LOW);
     }
- 
+    
   }
   
   stepper.moveStep();
   stepper2.moveStep();
-  //Serial.print("Is Move - stepper : ");
-  //Serial.print(stepper.MoveOnIs);
-  //Serial.print(", stepper2 : ");
-  //Serial.println(stepper2.MoveOnIs);
+
 }
